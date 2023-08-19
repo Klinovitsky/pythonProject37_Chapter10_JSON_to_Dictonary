46d8f7e2dcca95608cb1c7715339bf677d655bc2
Klinovitsky <klinovitsky@gmail.com>
2023-08-19 14:01:49 +0300

    json get

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 # Chapter 10. JSON
-# Test commit
 import json
 
+# Making a request to API
+import requests
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Output from iss-now API:\n", response.text)
+else:
+    print("Houston, we have a problem:", response.status_code)
+
+
+# Dictionaries
 current = {'temperature': 67.2,
            'precip_prob': '40%'}
 
