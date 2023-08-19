f4d9aa0a43f6c73f068c0619b8a1f4661c4b1b9d
Klinovitsky <klinovitsky@gmail.com>
2023-08-19 13:17:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+current = {'temperature': 67.2,
+           'precip_prob': '40%'}
+
+what_country = {'country': 'UK'}
+loc = {'city': 'London'}
+print('In',
+      loc['city'] + ', ' + what_country['country'] + ' it is ' + str(current['temperature']),
+      'degrees')
