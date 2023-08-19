8b009544ba60de410f670d4c15321b3e24918f00
Klinovitsky <klinovitsky@gmail.com>
2023-08-19 13:36:29 +0300

    into dictionary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Chapter 10. JSON
 # Test commit
+import json
 
 current = {'temperature': 67.2,
            'precip_prob': '40%'}
@@ -8,4 +9,10 @@ what_country = {'country': 'UK'}
 loc = {'city': 'London'}
 print('In',
       loc['city'] + ', ' + what_country['country'] + ' it is ' + str(current['temperature']),
-      'degrees')
+      'degrees\n')
+
+
+# From JSON into a dictionary
+json_string = '{"first": "Emmett", "last": "Brown", "prefix": "Dr"}'
+name = json.loads(json_string)
+print(name['prefix'], name['first'], name['last'])
